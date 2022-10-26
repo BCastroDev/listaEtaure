65b9ceec1bf90df34643da14f59dc4f5a34be4bd
Bruno Gráfica <servicosbruno@gmail.com>
2022-10-26 16:54:55 -0300

    adicionando db

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
+const pool = require('../db/db.js')
 
 router.post('/register',userController.register)
 router.post('/login', userController.login)
+
+router.get('/', async (req,res)=>{
+    try {
+        const users = await pool.query('SELECT * FROM users');
+        res.json({users : users.rows})
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+})
 
 module.exports = router;
