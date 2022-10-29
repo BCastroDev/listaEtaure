62d29ec6194dce2623b50353481cc50edb904a10
Bruno Carvalho <servicosbruno@gmail.com>
2022-10-29 12:36:23 -0300

    ejs

--- app.js
+++ app.js
@@ -8,14 +8,18 @@ const bodyParser = require('body-parser')
 const userRouter = require('./routes/userRouter')
 const authRouter = require('./routes/authRouter')
 
+app.set("view engine","ejs")
+
 dotenv.config();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use('/',userRouter)
+app.use('/', (req,res)=>{
+    res.render("index")
+})
 app.use('/user', userRouter)
 app.use('/auth', authRouter)
 
 
 
-app.listen(process.env.PORT, ()=>{console.log("Servidor rodando. http://localhost:" + process.env.PORT +"/users" )})
+app.listen(process.env.PORT, ()=>{console.log("Servidor rodando. http://localhost:" + process.env.PORT )})
